a47abe1e12e825136842193b1935c1e2a4349401
Selviniahh <xqwyss1699@gmail.com>
2024-06-21 00:00:31 -0400

    google benchmark added. But only useable for one cpp file for now

--- src/MainMain.cpp
+++ src/MainMain.cpp
@@ -1,6 +1,18 @@
-//This is temporary main file for libraries to link and include. I will not add anything here soon in the future.
-#include <iostream>
+#include <benchmark/benchmark.h>
 
-int main (){
- std::cout << "Hello world";
+static void BM_StringCreation(benchmark::State& state) {
+    for (auto _ : state)
+        std::string empty_string;
 }
+// Register the function as a benchmark
+BENCHMARK(BM_StringCreation);
+
+// Define another benchmark
+static void BM_StringCopy(benchmark::State& state) {
+    std::string x = "hello";
+    for (auto _ : state)
+        std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+BENCHMARK_MAIN();
