e8db1d8baaed0f1fcc8eaac28979b732a6d68270
Selviniahh <xqwyss1699@gmail.com>
2024-06-27 18:50:08 -0400

    moving on to the next question

--- src/Data Structures/Questions/Array/PairWithGivenSumMain.cpp
+++ src/Data Structures/Questions/Array/PairWithGivenSumMain.cpp
@@ -1,8 +1,10 @@
 //Given an array A[] of n numbers and another number x, the task is to check whether or not there exist two elements in A[] whose sum is exactly x. 
 //Input: arr[] = {0, -1, 2, -3, 1}, x= -2             1 + (-3) = -2
-
 #include <algorithm>
 #include <iostream>
+#include <unordered_set>
+
+using namespace std;
 
 //Simple but time complexity is: O(n*n)
 bool Solution1(int A[], int size, int x)
@@ -15,26 +17,42 @@ bool Solution1(int A[], int size, int x)
 }
 
 //Two pointer technique. Time Complexity: O(NlogN), Time complexity for sorting the array
-bool HasArrayTwoCandidates(int* A, const int arrSize, int sum)
+bool TwoPointer(int* A, const int arrSize, int sum)
 {
     int left = 0, right = arrSize - 1;
-
-    //sort the array first
-    std::sort(&A[0], &A[arrSize]);
-    while (left < right)
+    //Before anything sort
+    std::sort(A, A + arrSize);
+    while (right > left || left < right)
     {
-        if (A[left] + A[right] == sum)
-            return true;
-
-        if (A[left] + A[right] < sum) //need to go from left -> right
-            left++;
-        else
-            right--;
-    };
+        if (A[left] + A[right] == sum) return true;
+        if (A[left] + A[right] > sum) right--;
+        else if (A[left] + A[right] < sum) left++;
+    }
     return false;
 }
 
-int main()
+void printPairs(int arr[], int arr_size, int sum)
+{
+    std::unordered_set<int> s;
+    for (int i = 0; i < arr_size; i++)
+    {
+        int temp = sum - arr[i];
+        
+        if (s.find(temp) != s.end())
+        {
+            int first = temp;
+            int sec = arr[i];
+            int result = first + sec; 
+            std::cout << "Yes" << std::endl;
+            return;
+        }
+        s.insert(arr[i]);
+    }
+    std::cout << "No" << std::endl;
+}
+
+
+int main(int argc, char** argv)
 {
     //Solution 1 
     int A[] = {0, -1, 2, -3, 1};
@@ -46,5 +64,13 @@ int main()
     int A2[] = {1, 4, 45, 6, 10, -8};
     int x2 = 16;
     int arrSize = sizeof(A2) / sizeof(A2[0]);
-    HasArrayTwoCandidates(A2, arrSize, x2);
+    TwoPointer(A2, arrSize, x2);
+
+    //Solution 3
+    int A3[] = { 1, 4, 45, 6, 10, 8 };
+    const int n = 53;
+    const int arr_size = sizeof(A3) / sizeof(A[0]);
+
+    // Function calling
+    printPairs(A3, arr_size, n);
 }
